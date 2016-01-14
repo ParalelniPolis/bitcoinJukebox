d0bd6e43ab783639bc1e8dd4b55afc58a51c7d7e
tomtomklima <tomtom.klima@centrum.cz>
2016-01-14 22:31:47 +0100

    full needed info output withnout qr codes

--- SongParser.php
+++ SongParser.php
@@ -7,34 +7,45 @@
  * Time: 18:33
  */
 class SongParser {
-    private $path;
-    private $getid3;
-    /**
-     * SongParser constructor.
-     */
-    public function __construct($path) {
-        $this->getid3 = new getID3();
-        $this->path = $path;
-    }
+	private $getid3;
 
-    public function getSongsInfo() {
-        $files = scandir($this->path);
-        $result = [];
-        foreach($files as $file) {
-            if ($file == '.' || $file == '..') continue;
-            $result[] = $this->getid3->analyze($this->path.'/'.$file);
-        }
+	/**
+	 * SongParser constructor.
+	 */
+	public function __construct() {
+		$this->getid3 = new getID3();
+	}
 
-        $songs = [];
-        $i = 0;
-        foreach ($result as $s) {
-            if (!empty($s['id3v1']['title'])) $songs[$i]['title'] = $s['id3v1']['title']; else $songs[$i]['title'] = "unknown title";
-            if (!empty($s['id3v1']['artist'])) $songs[$i]['artist'] = $s['id3v1']['artist']; else $songs[$i]['artist'] = "unknown artist";
-            $songs[$i]['nameOfFile'] = $s['filename'];
-            $i++;
-        }
-        return $songs;
-    }
+	public function getSongsInfo($path) {
+		$files = scandir($path);
+		$result = [];
+		foreach ($files as $file) {
+			if ($file == '.' || $file == '..') continue;
+			if (is_dir($path.'/'.$file)) {
+				//need flat structure
+				$resultRecursion = $this->getSongsInfo($path.'/'.$file);
+				$result = array_merge($result, $resultRecursion);
+			} else { //if file!
+				$r = $this->getid3->analyze($path.'/'.$file);
+				$r['filenamepath'] = $path.'/'.$file;
+				$result[] = $r;
+			}
+		}
+		return $result;
+	}
+
+	public function parseSongsInfo($rawSongs) {
+		$songs = [];
+		$i = 0;
+		foreach ($rawSongs as $s) {
+			if (!empty($s['id3v1']['title'])) $songs[$i]['title'] = $s['id3v1']['title']; else $songs[$i]['title'] = '';
+			if (!empty($s['id3v1']['artist'])) $songs[$i]['artist'] = $s['id3v1']['artist']; else $songs[$i]['artist'] = '';
+			$songs[$i]['url'] = str_replace(' ', '%20', $s['filenamepath']);
+			$songs[$i]['nameOfFile'] = $s['filename'];
+			$i++;
+		}
+		return $songs;
+	}
 
 
 }
--- index.php
+++ index.php
@@ -8,16 +8,15 @@
 require_once ('SongParser.php');
 require_once ('getID3-1.9.10/getid3/getid3.php');
 
-$songParser = new SongParser('./songs');
-$songs = $songParser->getSongsInfo();
-
-header('Content-type: application/json');
-echo json_encode($songs);
-
-//require('view.php');
-//send json instead
+$songParser = new SongParser();
+$songs = $songParser->getSongsInfo('./songs');
+$songs = $songParser->parseSongsInfo($songs);
 
 
-//TODO QR addresses to single songs
-//TODO check payments via http://blockr.io/documentation/api
-//TODO song player (random and payed songs) res: http://www.webcodegeeks.com/html5/html5-audio-player-example/
+sendSongs($songs);
+
+function sendSongs($songs) {
+	header('Content-type: application/json');
+	//var_dump($songs);
+	echo json_encode($songs);
+}
