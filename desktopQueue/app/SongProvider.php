6cef112093c93ee9ea4a4c72f78f925824a57b2f
racinmat <racinmat@fel.cvut.cz>
2016-04-11 14:55:06 +0200

    Bugfix.

--- desktopQueue/app/SongProvider.php
+++ desktopQueue/app/SongProvider.php
@@ -1,7 +1,5 @@
 <?php
-
 require_once 'Song.php';
-
 /**
  * Created by PhpStorm.
  * User: Azathoth
@@ -10,22 +8,17 @@ require_once 'Song.php';
  */
 class SongProvider
 {
-
 	/** @var PDO */
 	private $connection;
-
 	/** @var string */
 	private $webSongsDir;
-
 	/** @var string */
 	private $filesystemSongsDir;
-
 	public function __construct(string $host, string $dbName, string $username, string $password, string $songsDirectory, string $webSongsDir, int $port) {
 		$this->connectToDatabase($host, $dbName, $username, $password, $port);
 		$this->webSongsDir = $webSongsDir;
 		$this->filesystemSongsDir = $songsDirectory;
 	}
-
 	private function connectToDatabase(string $host, string $dbName, string $username, string $password, int $port)
 	{
 		$dsn = "mysql:dbname=$dbName;host=$host;port=$port";
@@ -37,47 +30,44 @@ class SongProvider
 			throw new Exception('Connection failed: ' . $e->getMessage());
 		}
 	}
-
-
 	/**
 	 * @return Song[]
 	 */
 	public function readNonProcessedSongs() : array
 	{
-		$stmt = $this->connection->prepare('SELECT song.id, song.name, queue.id, song.album_cover AS queueId FROM song JOIN queue ON song.id = queue.song WHERE queue.paid = TRUE AND queue.proceeded = FALSE');
+		$stmt = $this->connection->prepare('SELECT song.id, song.name, queue.id AS queueId, song.album_cover, song.artist, song.title, song.duration FROM song JOIN queue ON song.id = queue.song WHERE queue.paid = TRUE AND queue.proceeded = FALSE');
 		$stmt->execute();
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		var_dump($result);
-
 		$queueIds = array_column($result, 'queueId');
-
 		/** @var Song[] $data */
 		$data = [];
 		foreach ($result as $songData) {
-			$song = new Song($songData['name'], $this->webSongsDir. '/' . $songData['id'], $this->filesystemSongsDir. '/' . $songData['id'], $songData['album_cover']);
+			$song = new Song($songData['name'], "$this->webSongsDir/{$songData['id']}.mp3", "$this->filesystemSongsDir/{$songData['id']}.mp3", $songData['album_cover'], $songData['artist'], $songData['title'], $songData['duration']);
 			$data[] = $song;
 		}
-
 		var_dump($data);
-
 		if (count($queueIds) > 0) {
 			$quotedIds = array_map(function($id) {return $this->connection->quote($id);}, $queueIds);
 			$stmt = $this->connection->prepare('UPDATE queue SET proceeded = TRUE WHERE id IN ('. implode(',', $quotedIds) . ')');
 			echo $stmt->queryString . PHP_EOL;
 			$stmt->execute();
 		}
-
 		return $data;
 	}
-
 	public function getRandomSong(int $genreId) : Song
 	{
-		$stmt = $this->connection->prepare('SELECT song.id, song.name, song.album_cover FROM song WHERE genre_id = :genreId ORDER BY RAND() LIMIT 1');
+		//todo: ošetřit, aby bylo možno objednávat jen žánry s alespoň jednou písní
+		$stmt = $this->connection->prepare('SELECT song.id, song.name, song.album_cover, song.artist, song.title, song.duration FROM song WHERE genre_id = :genreId ORDER BY RAND() LIMIT 1');
 		$stmt->execute(['genreId' => $genreId]);
 		$songData = $stmt->fetch(PDO::FETCH_ASSOC);
+		if (!$songData) {   //vybraný žánr nemá žádnou skladbu
+			$stmt = $this->connection->prepare('SELECT song.id, song.name, song.album_cover FROM song ORDER BY RAND() LIMIT 1');
+			$stmt->execute();
+			$songData = $stmt->fetch(PDO::FETCH_ASSOC);
+		}
 		echo 'song name: ' . $songData['name'] .  ', album cover:' . $songData['album_cover'] . PHP_EOL;
-		$song = new Song($songData['name'], $this->webSongsDir. '/' . $songData['id'], $this->filesystemSongsDir. '/' . $songData['id'], $songData['album_cover']);
+		$song = new Song($songData['name'], "$this->webSongsDir/{$songData['id']}.mp3", "$this->filesystemSongsDir/{$songData['id']}.mp3", $songData['album_cover'], $songData['artist'], $songData['title'], $songData['duration']);
 		return $song;
 	}
-
 }
